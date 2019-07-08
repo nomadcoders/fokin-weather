c9cab56ab2cae844b6a8078d894ef22c5d03abed
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-07-08 14:20:21 +0900

    #0.5 How does React Native Work?

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello!!!</Text>
     </View>
   );
 }
@@ -12,8 +12,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  text: {
+    color: "white"
+  }
 });
