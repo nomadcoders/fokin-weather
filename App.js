5588fde6ea2d673305af19ff76a717ada876cddc
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-07-08 14:37:42 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,25 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView} />
-      <View style={styles.blueView} />
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow"
-  },
-  blueView: {
-    flex: 3,
-    backgroundColor: "blue"
-  }
-});
---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Getting the fucking weather</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FDF6AA"
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 30
+  }
+});
